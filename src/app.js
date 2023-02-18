48a6b659b50589eef329bb7c84d9520b7ad983fd
dlimaf <dlimaf1995@gmail.com>
2023-02-18 14:17:35 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var extensions = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let m = 0; m < extensions.length; m++) {
+          let nuevo = document.createElement("div");
+          nuevo.innerHTML = pronoun[i] + adj[j] + noun[k] + extensions[m];
+          document.querySelector("p").appendChild(nuevo);
+          console.log(nuevo.innerHTML);
+        }
+      }
+    }
+  }
 };
